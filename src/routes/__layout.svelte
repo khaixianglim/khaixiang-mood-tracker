453e4369ea40541742ed27cb37a7e11a57a051da
khaixianglim <khaixiang12122@gmail.com>
2021-12-12 10:22:31 +0000

    project code updated

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,32 +1,34 @@
-<!-- JS -->
 <script>
-	import supabase from '$lib/db.js';
-	import { page, session } from '$app/stores';
-	import { browser } from '$app/env';
-	import { goto } from '$app/navigation';
-	import Navbar from '$lib/Navbar.svelte';
-	// code that runs only in the browser
-	if (browser) {
-		$session = supabase.auth.session(); // set session
-		redirect();
+    import Navbar from '$lib/Navbar.svelte';
+    import supabase from '$lib/db';
+    import { page, session } from '$app/stores';
+    import { browser } from '$app/env';
+    import { goto } from '$app/navigation';
 
-		supabase.auth.onAuthStateChange((userSession) => {
-			$session = userSession; // set session
-			redirect();
-		});
-	}
-	function redirect() {
-		//login redirect
-		if ($session && $page.path === '/login') {
-			goto('/');
-		}
-		//logout redirect
-		if (!$session && $page.path === '/') {
-			goto('/login');
-		}
-	}
+    // code that runs only in the browser
+    if (browser) {
+   	 $session = supabase.auth.session(); // set session
+   	 redirect();
+
+   	 supabase.auth.onAuthStateChange((event, userSession) => {
+   		 $session = userSession; // set session
+   		 redirect();
+   	 });
+    }
+
+    function redirect() {
+   	 //login redirect
+   	 if ($session && $page.path === '/login') {
+   		 goto('/');
+   	 }
+
+   	 //logout redirect
+   	 if (!$session && $page.path === '/') {
+   		 goto('/login');
+   	 }
+    }
 </script>
 
-<!-- HTML -->
 <Navbar />
+
 <slot />
